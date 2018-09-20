2a98b1628e9294bf80dcdb111d4eecf9f8cd064a
Daniel Berei <berei.daniel@gmail.com>
2018-09-20 12:26:51 +0200

    feat(server running, /posts endpoint works)

--- main.js
+++ main.js
@@ -3,10 +3,31 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Bereid+123',
+  database: 'reddit',
+});
 
-app.get('/hello', (req, res) => {
-  res.send('hello world');
-})
+connection.connect((err) => {
+  if (err) {
+    console.log('Connection failed to database: ' + err);
+    return;
+  }
+  console.log('Connection established');
+});
+
+app.get('/post', (req, res) => {
+  connection.query(`SELECT * FROM posts`, (err, result) => {
+      if(err){
+        res.status(500).send('Database error')
+      }
+      res.json(result);
+    }
+  )
+});
 
 app.listen(PORT, ()=> {
   console.log(`Server is running on the PORT: ${PORT}`);
