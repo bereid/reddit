fc5df7802cdba6ca7c610370766af660ad7ab98f
Daniel Berei <berei.daniel@gmail.com>
2018-09-28 11:55:47 +0200

    feat(new post has two divs: voter and content)

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,24 @@
 window.onload = () => {
   const host = 'http://localhost:8080'
-  const maincontent = document.querySelector('main-content');
+  const maincontent = document.querySelector('.main-content');
   const http = new XMLHttpRequest();
   http.open('GET', `${host}/posts`, true);
   http.onload = () => {
     let postList = JSON.parse(http.response);
-    console.log(postList);
+    postList.forEach(post => {
+      let newPostDiv = document.createElement('div');
+      newPostDiv.classList.add('post');
+      maincontent.appendChild(newPostDiv);
+      
+      let voter = document.createElement('div');
+      voter.classList.add('voter');
+      newPostDiv.appendChild(voter);
+      
+      let postContent = document.createElement('div');
+      postContent.classList.add('postcontent');
+      newPostDiv.appendChild(postContent);
+
+    });
   }
   http.send(); 
 }
