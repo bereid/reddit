1ba26f097ec524c89679c596a68ba698aea0229a
Daniel Berei <berei.daniel@gmail.com>
2018-09-30 00:24:23 +0200

    feat(upvote and downvote works)

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,30 @@
 window.onload = () => {
   const host = 'http://localhost:8080'
   const maincontent = document.querySelector('.main-content');
-  const http = new XMLHttpRequest();
-  const downVoteImg = 'assets/downvote.png';
-  const upVoteImg = 'assets/upvote.png';
   const submitButton = document.querySelector('.submit');
-  http.open('GET', `${host}/posts`, true);
-  http.onload = () => {
-    let postList = JSON.parse(http.response);
+  const getPosts = new XMLHttpRequest();
+  const upvotePosts = new XMLHttpRequest();
+  const downvotePosts = new XMLHttpRequest();
+  const downVoteImg = 'assets/downvote.png';
+  const downVotedImg = 'assets/downvoted.png';
+  const upVoteImg = 'assets/upvote.png';
+  const upVotedImg = 'assets/upvoted.png';
+  const upvote = (postid) => {
+    upvotePosts.open('PUT', `${host}/posts/${postid}/upvote`, true);
+    upvotePosts.onload = () => {
+    }
+    upvotePosts.send();
+  }
+  const downvote = (postid) => {
+    upvotePosts.open('PUT', `${host}/posts/${postid}/downvote`, true);
+    upvotePosts.onload = () => {
+    }
+    upvotePosts.send();
+  }
+
+  getPosts.open('GET', `${host}/posts`, true);
+  getPosts.onload = () => {
+    let postList = JSON.parse(getPosts.response);
     postList.forEach(post => {
       let newPostDiv = document.createElement('div');
       newPostDiv.classList.add('post');
@@ -24,8 +41,15 @@ window.onload = () => {
       let upVoter = document.createElement('input');
       upVoter.type = 'image';
       upVoter.src = upVoteImg;
-      upVoter.alt = 'submit'
+      upVoter.alt = 'submit';
+      upVoter.classList.add('upvote-button');
       voter.appendChild(upVoter);
+
+      upVoter.addEventListener('click', () => {
+        upVoter.src = upVotedImg;
+        upvote(post.id);
+        voteNumber.innerText = (Number(voteNumber.innerText) + 1);
+      });
 
       let voteNumber = document.createElement('p');
       voteNumber.classList.add('votenumber');
@@ -36,7 +60,14 @@ window.onload = () => {
       downVoter.type = 'image';
       downVoter.src = downVoteImg;
       downVoter.alt = 'submit';
+      downVoter.classList.add('downvote-button');
       voter.appendChild(downVoter);
+
+      downVoter.addEventListener('click', () => {
+        downVoter.src = downVotedImg;
+        downvote(post.id);
+        voteNumber.innerText = (Number(voteNumber.innerText) - 1);
+      });
 
       let postLink = document.createElement('a');
       postLink.classList.add('postlink')
@@ -49,17 +80,17 @@ window.onload = () => {
       let currentDate = new Date();
       let currentDateInNumberTimeStamp = Date.parse(currentDate);
       let postDateInNumberTimeStamp = Date.parse(post.date);
-      let elapsedTime = 
-      Math.floor((currentDateInNumberTimeStamp-postDateInNumberTimeStamp)/3600000);
+      let elapsedTime =
+        Math.floor((currentDateInNumberTimeStamp - postDateInNumberTimeStamp) / 3600000);
       submitDate.innerHTML = `submitted ${elapsedTime} hours ago by ${post.owner}`;
       postContent.appendChild(submitDate);
 
     });
 
     submitButton.addEventListener('click', () => {
-      location.href=`${host}/add-post`;
+      location.href = `${host}/add-post`;
     });
 
   }
-  http.send();
+  getPosts.send();
 }
